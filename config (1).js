23c79136e62d1466858e73a3b3bb2ab7a683b858
maximeansah <maximeansah@gmail.com>
2026-05-16 11:09:23 +0000

    Add files via upload

---
+++ config (1).js
@@ -0,0 +1,18 @@
+const siteConfig = {
+  whatsapp: "233559896598",
+
+  tagline: "Personalised tutoring that builds confidence and guarantees academic improvement.",
+
+  subjects: [
+    "Mathematics",
+    "Science",
+    "English",
+    "Social Studies",
+    "ICT"
+  ],
+
+  pricing: {
+    weekly: "65 GHS",
+    monthly: "160 GHS"
+  }
+};
